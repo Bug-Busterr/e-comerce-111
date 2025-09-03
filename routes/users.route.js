a62b9812644b22f833b02e8cac0c5eaff3ce8bba
Hadeer Mostafa <Hadeer.Mostafa00@outlook.com>
2025-09-03 22:31:12 +0300

    Refactor validation schemas and user controller: - Added password update functionality with validation. - Improved product validation messages for clarity. - Enhanced order routes with user role protection. - Updated user routes to include password update and profile retrieval.

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,60 +1,66 @@
 import express from "express";
-import multer from 'multer';
-import AppError from "../utils/errors/appError.js"
+import multer from "multer";
+import AppError from "../utils/errors/appError.js";
+import { allowedTo } from "../middleware/allowedTo.js";
+import { userRoles } from "../utils/userRoles.js";
 import {
-    getAllUsers,
-    register,
-    login,
-    signUpWithGoogle,
-    userProfile,
-    forgetPassword
+  getAllUsers,
+  register,
+  login,
+  userProfile,
+  forgetPassword,
+  updatePassword,
 } from "../controllers/users.controller.js";
 import { validate } from "../middleware/validate.js";
 import { SignUpSchema } from "../Validation/validationSchema.js";
+import { updatePasswordSchema } from "../Validation/validationSchema.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-
 const diskStorage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null , 'uploads'),
-    filename: (req , file , cb) =>{
-        const extension = file.mimetype.split('/')[1];
-        const fileName = `user-${Date.now()}.${extension}`;
-        cb(null, fileName);
-    }
-})
-
-const fileFilter = (req, file, cb) => {
-    const imageType = file.mimetype.split('/')[0];
-    if(imageType === 'image') {
-        return cb(null, true)
-    } else {
-        return cb(AppError.createError('file must be an image', 400 , "Fail"), false)
-    }
-}
-
-const upload = multer({
-    storage: diskStorage,
-    fileFilter
+  destination: (req, file, cb) => cb(null, "uploads"),
+  filename: (req, file, cb) => {
+    const extension = file.mimetype.split("/")[1];
+    const fileName = `user-${Date.now()}.${extension}`;
+    cb(null, fileName);
+  },
 });
 
-router.route('/')
-            .get(getAllUsers)
+const fileFilter = (req, file, cb) => {
+  const imageType = file.mimetype.split("/")[0];
+  if (imageType === "image") {
+    return cb(null, true);
+  } else {
+    return cb(
+      AppError.createError("file must be an image", 400, "Fail"),
+      false
+    );
+  }
+};
 
-router.route('/register')
-            .post(validate(SignUpSchema),upload.single('avatar') , register)
+const upload = multer({
+  storage: diskStorage,
+  fileFilter,
+});
 
-router.route('/login')
-            .post(login)
+router.route("/").get(getAllUsers);
 
-router.route('/signupwithgoogle')
-            .post(signUpWithGoogle)
+router
+  .route("/register")
+  .post(validate(SignUpSchema), upload.single("avatar"), register);
 
-router.route('/forgotPassword')
-            .patch(forgetPassword)
+router.route("/login")
+      .post(login);
 
-router.route('/me')
-            .patch(userProfile)
+router.route("/forgotPassword")
+      .patch(forgetPassword);
 
+router.route("/me")
+      .patch(protect, allowedTo(userRoles.USER), userProfile);
+
+router
+  .route("/updatePassword")
+  .post(protect,allowedTo(userRoles.USER),validate(updatePasswordSchema),updatePassword);
 
 export default router;
